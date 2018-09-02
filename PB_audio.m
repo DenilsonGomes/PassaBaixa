2cd26c74bc714079dde5416a1bad0012bbbbf508
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-09-02 16:29:03 -0300

    Codigo atualizado

--- PB_audio.m
+++ PB_audio.m
@@ -1,21 +1,29 @@
-clear all
+% Autor: CARLOS ALEXANDRE ROLIM FERNANDES
+% Revisado por: Denilson Gomes Vaz da Silva
+% Departamento de Engenharia de Computação
+%
+% Script para exemplificar filtro passa baixa
+
+clear
 close all
 clc
 
-[y,Fs]= audioread('botao.wav');
-% load train  
+[y,Fs]= audioread('botao.wav'); %Carrega o audio
+% load train  %Carrega o audio
+% load chirp  %Carrega o audio
+
 y = y(:,1);
 y = y(1:5:end);
 Fs = Fs/5;
-figure,plot(y)
-tempo_vec = (1:length(y));
+plot(y) %Plota o sinal
+tempo_vec = (1:length(y)); %tempo
 ylabel('Amplitude do Sinal')
 xlabel('Tempo discreto')
 
 % Transformada de Fourier
-Yw = fftshift(fft(y));
-freq_vec = linspace(-pi,pi,length(y));
-figure,plot(freq_vec,abs(Yw),'b')
+Yw = fftshift(fft(y)); %TF
+freq_vec = linspace(-pi,pi,length(y)); %Frequencias
+figure,plot(freq_vec,abs(Yw),'b') %Plota a TF do sinal
 ylabel('Espectro de magnitude')
 xlabel('Frquencia em Rad/se')
 
@@ -23,12 +31,12 @@ xlabel('Frquencia em Rad/se')
 
 
 % Filtragem
-wc1 = 0.2*pi;
+wc1 = 0.08*pi; %Frquencia de conrte
 [B,A] = butter(10, wc1/pi);
 [H,W] = freqz(B,A,length(y));
-figure,plot(W,abs(H))
+figure,plot(W,abs(H)) %Plota filtro Passa-Baixa
 y_filt = filter(B,A,y);
-figure,plot(real(y_filt))
+figure,plot(real(y_filt)) %Plota o sinal filtrado
 ylabel('Amplitude do Sinal Filtrado')
 xlabel('Tempo discreto')
 % axis([0 6e4 -1.1 1.1])
@@ -38,8 +46,6 @@ xlabel('Tempo discreto')
 
 Yw_filt = fftshift(fft(y_filt));
 freq_vec = linspace(-pi,pi,length(Yw_filt));
-figure,plot(freq_vec,abs(Yw_filt))
+figure,plot(freq_vec,abs(Yw_filt)) %Plota TF do sinal filtrado
 ylabel('Espectro de magnitude do Sinal Filtrado')
 xlabel('Frquencia em Rad/s')
-
-
